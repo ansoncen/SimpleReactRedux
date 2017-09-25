ae1d7d0a5926758761b74619a48c483a8a93d210
sihan Cen <censihan@gmail.com>
2017-09-25 15:20:33 +1000

    push project

---
+++ src/containers/book_detail.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+    render(){
+        if(!this.props.book){
+            return <div>Select a book to get detail.</div>;
+        }
+
+        return (
+            <div>
+                <h3>Details for book</h3>
+                <div>Title: { this.props.book.title }</div>
+                <div>Page: { this.props.book.page }</div>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        book: state.activeBook
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return[
+        { title: 'book name 1', page: '111',},
+        { title: 'book name 2', page: '222',},
+        { title: 'book name 3', page: '333',},
+        { title: 'book name 4', page: '444',},
+        { title: 'book name 5', page: '555',},
+        { title: 'book name 6', page: '666',},
+    ]
+}
